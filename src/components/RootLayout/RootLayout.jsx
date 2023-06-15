6d34d95d4c933e30c2172ab896706d446bfdac97
amramos <ramosandersonmay@gmail.com>
2023-06-15 17:03:25 +0200

    F1.S1.B2 - my stories was not loading unless the user went to search page first

--- src/components/RootLayout/RootLayout.jsx
+++ src/components/RootLayout/RootLayout.jsx
@@ -2,8 +2,16 @@ import { Outlet } from 'react-router-dom';
 import NavbarPanel from '../NavbarPanel/NavbarPanel';
 import { Provider } from 'react-redux';
 import store from '../../store/store';
+import { useDispatch } from 'react-redux';
+import { getMovieGenres } from "../../store/reducers/movieGenreSlice";
+import { getUserMovies } from "../../store/reducers/userMoviesSlice";
 
 const RootLayout = () => {
+
+    const dispatch = useDispatch();
+
+    dispatch(getMovieGenres());
+    dispatch(getUserMovies("amramos"));
 
     return (
         <>
--- src/pages/StorySearch/StorySearch.jsx
+++ src/pages/StorySearch/StorySearch.jsx
@@ -1,16 +1,7 @@
 import Movies from "../../components/Movies/Movies";
 import StorySearchForm from "./StorySearchForm";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { getMovieGenres } from "../../store/reducers/movieGenreSlice";
-import { getUserMovies } from "../../store/reducers/userMoviesSlice";
 
 const StorySearch = () => {
-
-    const dispatch = useDispatch();
-
-    dispatch(getMovieGenres());
-    dispatch(getUserMovies("amramos"));
 
     return (
         <>
